d39181fad612f020fd389a6203f607540438a300
NemoTaek <vkfkdchfhr@gmail.com>
2021-01-06 18:04:01 +0900

    키보드 이벤트 추가

--- ts-project/src/App.tsx
+++ ts-project/src/App.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import './App.css';
 
-import Greetings from './component/greet';
 
 function App() {
-  const onClick = (name: string) => {
-    console.log(`${name} says hello`);
-  };
-  return <Greetings name="React" click={onClick} />;
+
+  let x_position: number = 100;
+  let y_position: number = 100;
+
+  window.addEventListener('keydown', (e: any) => {
+    let character: any = document.getElementsByClassName('character')[0];
+
+    if (e.code === 'ArrowRight') {
+      x_position += 10;
+    }
+    else if (e.code === 'ArrowLeft') {
+      x_position -= 10;
+    }
+    else if (e.code === 'ArrowUp') {
+      y_position -= 10;
+    }
+    else if (e.code === 'ArrowDown') {
+      y_position += 10;
+    }
+
+    character.style.transform = `translate(${x_position}px, ${y_position}px)`;
+    console.log(x_position)
+    console.log(y_position)
+  })
+  return (
+    <div className="background">
+      <div className="character"></div>
+    </div>
+  );
 }
 
 export default App;
