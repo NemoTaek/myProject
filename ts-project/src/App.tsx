8f3dce4f5c533073b25d6ce8dffcba32a52c9948
NemoTaek <vkfkdchfhr@gmail.com>
2021-01-07 17:56:33 +0900

    키보드 이벤트 추가

--- ts-project/src/App.tsx
+++ ts-project/src/App.tsx
@@ -1,35 +1,65 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 
-
 function App() {
+  const speed: number = 5;
+  const size: number = 10;
+  const keyPress: Object = {}
+  let x_position: number = 0;
+  let y_position: number = 0;
 
-  let x_position: number = 100;
-  let y_position: number = 100;
+  useEffect(() => {
+    const character: any = document.querySelector('#character');
+    console.log(x_position, y_position)
+
+    setInterval(function (e: any) {
+      if (keyPress['ArrowRight']) {
+        if (x_position >= Math.floor(window.innerWidth - window.innerWidth / (100 / size))) {
+          x_position = Math.floor(window.innerWidth - window.innerWidth / (100 / size));
+        }
+        else {
+          x_position += speed;
+        }
+      }
+      else if (keyPress['ArrowLeft']) {
+        if (x_position <= 0) {
+          x_position = 0;
+        }
+        else {
+          x_position -= speed;
+        }
+      }
+      else if (keyPress['ArrowUp']) {
+        if (y_position <= 0) {
+          y_position = 0;
+        }
+        else {
+          y_position -= speed;
+        }
+      }
+      else if (keyPress['ArrowDown']) {
+        if (y_position >= Math.floor(window.innerHeight - window.innerWidth / (100 / size))) {
+          y_position = Math.floor(window.innerHeight - window.innerWidth / (100 / size));
+        }
+        else {
+          y_position += speed;
+        }
+      }
+
+      character.style.transform = `translate(${x_position}px, ${y_position}px)`;
+    }, 10)
+  })
 
   window.addEventListener('keydown', (e: any) => {
-    let character: any = document.getElementsByClassName('character')[0];
-
-    if (e.code === 'ArrowRight') {
-      x_position += 10;
-    }
-    else if (e.code === 'ArrowLeft') {
-      x_position -= 10;
-    }
-    else if (e.code === 'ArrowUp') {
-      y_position -= 10;
-    }
-    else if (e.code === 'ArrowDown') {
-      y_position += 10;
-    }
-
-    character.style.transform = `translate(${x_position}px, ${y_position}px)`;
-    console.log(x_position)
-    console.log(y_position)
+    keyPress[e.code] = true;
   })
+  window.addEventListener('keyup', (e: any) => {
+    keyPress[e.code] = false;
+  })
+
   return (
-    <div className="background">
-      <div className="character"></div>
+    <div id="background">
+      <div id="character"></div>
     </div>
   );
 }
