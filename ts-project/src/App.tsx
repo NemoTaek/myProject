26e9cb5f2d2d4e471b87a72e7bb6e2ebd82c4569
NemoTaek <vkfkdchfhr@gmail.com>
2021-01-08 17:37:18 +0900

    미로찾기 제작 중 망함

--- ts-project/src/App.tsx
+++ ts-project/src/App.tsx
@@ -1,61 +1,79 @@
 import React, { useEffect } from 'react';
 import './App.css';
 
+let location: { x: number, y: number } = {
+  x: 50,
+  y: 50
+}
+
 function App() {
   const speed: number = 5;
-  const size: number = 10;
-  const keyPress: Object = {}
-  let x_position: number = 0;
-  let y_position: number = 0;
+  const size: number = 50;
+  const keyPress: object = {}
+
 
   useEffect(() => {
     const character: any = document.querySelector('#character');
-    console.log(x_position, y_position)
 
-    setInterval(function (e: any) {
-      if (keyPress['ArrowRight']) {
-        if (x_position >= Math.floor(window.innerWidth - window.innerWidth / (100 / size))) {
-          x_position = Math.floor(window.innerWidth - window.innerWidth / (100 / size));
+    window.addEventListener('keydown', (e: any) => {
+      // keyPress[e.code] = true;
+      if (e.code === 'ArrowRight') {
+        if (location.x >= Math.floor(window.innerWidth - size * 2)) {
+          location.x = Math.floor(window.innerWidth - size * 2);
         }
         else {
-          x_position += speed;
-        }
-      }
-      else if (keyPress['ArrowLeft']) {
-        if (x_position <= 0) {
-          x_position = 0;
-        }
-        else {
-          x_position -= speed;
-        }
-      }
-      else if (keyPress['ArrowUp']) {
-        if (y_position <= 0) {
-          y_position = 0;
-        }
-        else {
-          y_position -= speed;
-        }
-      }
-      else if (keyPress['ArrowDown']) {
-        if (y_position >= Math.floor(window.innerHeight - window.innerWidth / (100 / size))) {
-          y_position = Math.floor(window.innerHeight - window.innerWidth / (100 / size));
-        }
-        else {
-          y_position += speed;
-        }
-      }
+          setInterval(() => {
+            location.x += size;
+            location.y += 0;
+            character.style.transform = `translate(${location.x}px, ${location.y}px)`;
+          }, 500)
 
-      character.style.transform = `translate(${x_position}px, ${y_position}px)`;
-    }, 10)
+        }
+      }
+      else if (e.code === 'ArrowLeft') {
+        if (location.x <= 50) {
+          location.x = 50;
+        }
+        else {
+          setInterval(() => {
+            location.x -= size;
+            location.y += 0;
+            character.style.transform = `translate(${location.x}px, ${location.y}px)`;
+          }, 500)
+        }
+      }
+      else if (e.code === 'ArrowUp') {
+        if (location.y <= 50) {
+          location.y = 50;
+        }
+        else {
+          setInterval(() => {
+            location.x += 0;
+            location.y -= size;
+            character.style.transform = `translate(${location.x}px, ${location.y}px)`;
+          }, 500)
+        }
+      }
+      else if (e.code === 'ArrowDown') {
+        if (location.y >= Math.floor(window.innerHeight - size * 2)) {
+          location.y = Math.floor(window.innerHeight - size * 2);
+        }
+        else {
+          setInterval(() => {
+            location.x += 0;
+            location.y += size;
+            character.style.transform = `translate(${location.x}px, ${location.y}px)`;
+          }, 500)
+        }
+      }
+      console.log(location)
+    })
   })
 
-  window.addEventListener('keydown', (e: any) => {
-    keyPress[e.code] = true;
-  })
-  window.addEventListener('keyup', (e: any) => {
-    keyPress[e.code] = false;
-  })
+
+  // window.addEventListener('keyup', (e: any) => {
+  //   keyPress[e.code] = false;
+  // })
 
   return (
     <div id="background">
---
+++ ts-project/src/component/MazeMap/MazeMap.tsx
@@ -0,0 +1,19 @@
+import React, { useEffect } from 'react';
+import './MazeMap.css';
+
+function MazeMap() {
+
+
+  useEffect(() => {
+    let drawMap: HTMLElement = document.querySelector(".maze_map");
+
+  })
+
+  return (
+    <div className="maze_map">
+    </div>
+  );
+}
+
+
+export default MazeMap;
