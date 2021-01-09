230b23033b1c730901f99c404c7428ea7a68ec43
NemoTaek <vkfkdchfhr@gmail.com>
2021-01-09 18:52:06 +0900

    테트리스로 변경

--- ts-project/src/App.tsx
+++ ts-project/src/App.tsx
@@ -1,83 +1,17 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import './App.css';
 
-let location: { x: number, y: number } = {
-  x: 50,
-  y: 50
-}
-
 function App() {
-  const speed: number = 5;
-  const size: number = 50;
-  const keyPress: object = {}
-
+  let canvasRef = useRef(null);
 
   useEffect(() => {
-    const character: any = document.querySelector('#character');
-
-    window.addEventListener('keydown', (e: any) => {
-      // keyPress[e.code] = true;
-      if (e.code === 'ArrowRight') {
-        if (location.x >= Math.floor(window.innerWidth - size * 2)) {
-          location.x = Math.floor(window.innerWidth - size * 2);
-        }
-        else {
-          setInterval(() => {
-            location.x += size;
-            location.y += 0;
-            character.style.transform = `translate(${location.x}px, ${location.y}px)`;
-          }, 500)
-
-        }
-      }
-      else if (e.code === 'ArrowLeft') {
-        if (location.x <= 50) {
-          location.x = 50;
-        }
-        else {
-          setInterval(() => {
-            location.x -= size;
-            location.y += 0;
-            character.style.transform = `translate(${location.x}px, ${location.y}px)`;
-          }, 500)
-        }
-      }
-      else if (e.code === 'ArrowUp') {
-        if (location.y <= 50) {
-          location.y = 50;
-        }
-        else {
-          setInterval(() => {
-            location.x += 0;
-            location.y -= size;
-            character.style.transform = `translate(${location.x}px, ${location.y}px)`;
-          }, 500)
-        }
-      }
-      else if (e.code === 'ArrowDown') {
-        if (location.y >= Math.floor(window.innerHeight - size * 2)) {
-          location.y = Math.floor(window.innerHeight - size * 2);
-        }
-        else {
-          setInterval(() => {
-            location.x += 0;
-            location.y += size;
-            character.style.transform = `translate(${location.x}px, ${location.y}px)`;
-          }, 500)
-        }
-      }
-      console.log(location)
-    })
-  })
-
-
-  // window.addEventListener('keyup', (e: any) => {
-  //   keyPress[e.code] = false;
-  // })
+    let canvas = canvasRef.current;
+    let ctx = canvas.getContext('2d');
+  }, [])
 
   return (
     <div id="background">
-      <div id="character"></div>
+      <canvas id="tetris_map" ref={canvasRef} width={350} height={700} style={{ backgroundColor: "black" }}></canvas>
     </div>
   );
 }
